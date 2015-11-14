7df5a85176e7d2850bc5afb03de7f0bf39b4c863
edwardgaudio <edwardgaudio@gmail.com>
2015-11-14 14:22:33 -0500

    Initial Commit

---
+++ swift-libTests/swift_libTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_libTests.swift
+//  swift-libTests
+//
+//  Created by edward on 11/14/15.
+//  Copyright © 2015 edward gaudio. All rights reserved.
+//
+
+import XCTest
+@testable import swift_lib
+
+class swift_libTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
